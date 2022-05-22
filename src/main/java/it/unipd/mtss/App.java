63cf4998f4179a356bbcad00a43514587b718ff7
nicola <nicola.lazzarin89@gmail.com>
2022-05-22 13:26:31 +0200

    Maven_Setup

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Nicola] [Lazzarin] [1204686]
+// [Irene] [Benetazzo] [1223865]
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
